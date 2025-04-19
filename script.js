24f7a22a090e1f9624d52d93ee13e1b8732d172d
Guilherme da Silva Marchioli <marchioliprogrammer@gmail.com>
2025-04-19 13:43:46 -0300

    inicion da estilização

---
+++ script.js
@@ -0,0 +1,15 @@
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+    itens.style.display = 'none'
+    } else {
+        itens.style.display = 'none'
+    }
+}
+
+function clickMenu() {
+   if (itens.style.display == 'block') {
+    itens.style.display = 'none'
+   } else {
+    itens.style.display = 'block'
+   }
+}
